5120fb5edd784f505284c2d360ba0119d2820ccd
Bill Ramírez <bilyalon@gmail.com>
2022-05-31 09:52:50 -0600

    Project Done!

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ window.addEventListener("load", setDefaultGrid);
 
 let actualColor = "black";
 
-const btnChangeRange = document.querySelector("#change-size");
 const gridContainer = document.querySelector("#grid-container");
+const btnChangeRange = document.querySelector("#change-size");
 const btnRandomColor = document.querySelector("#random-buttom");
 const btnBlack = document.querySelector("#black-button");
+const resetbtn = document.querySelector("#reset-buttom");
 
-btnChangeRange.addEventListener("click", changeGridSize); 
+btnChangeRange.addEventListener("click", changeGridSize);
 btnBlack.addEventListener("click", chooseBlack);
 btnRandomColor.addEventListener("click", chooseRandom);
-
-
+resetbtn.addEventListener("click", reset);
 
 function changeGridSize() {
-  let newSize = prompt("Enter new size");
+  let newSize = prompt("Enter new size < 65");
   newSize = parseInt(newSize);
   if (newSize < 1 || newSize > 64 || Number.isNaN(newSize)) {
     alert("Enter a number from 1-64 range");
@@ -30,13 +30,13 @@ function setDefaultGrid() {
 }
 
 function makeGrid(size) {
-  let rows = size * size; 
+  let rows = size * size;
   for (let i = 0; i < rows; i++) {
-    const items = document.createElement("div"); 
-    items.className = "gridElement";
+    const items = document.createElement("div");
+    items.classList = "gridElement";
     items.addEventListener("mouseover", changeColor);
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`; 
-    gridContainer.appendChild(items); 
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.appendChild(items);
   }
 }
 
@@ -65,4 +65,12 @@ function RandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+
+function reset() {
+  let gridSquares = document.getElementsByClassName("gridElement");
+  let i = gridSquares.length;
+  while (i--) {
+    gridSquares[i].style.backgroundColor = "white";
+  }
 }
